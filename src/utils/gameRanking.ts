a5c922f21d491ba44ab55fc5054a60230ab4e8c7
Ryan Noelk <ryannoelk@gmail.com>
2025-07-05 07:43:34 +0200

    Adding comments for ranking

--- src/utils/gameRanking.ts
+++ src/utils/gameRanking.ts
@@ -1,13 +1,25 @@
 // @ts-nocheck
 import type { NHLGameScore, RankingResult } from '../types/types.ts';
 
-// Things to consider
-// fav team wins
-// 2 fav teams playing
-// phyical game
-// fighting
-// high shoot total for fav team
-// overtime/shootout
+/*
+Things to consider when ranking games:
+1. each game ranking mechinism should be in it's own file/funciton. THis allos for plug and play with the different rankings that you want to use.
+2. +50 points if a fav team wins.
+3. +50 points the ame includes one of your fav teams.
+4. -100 points if your fav team loses by more than 3 points. But only if the fav team is not playing againt another fav team.
+5. +50 points in the game went to overtime.
+6. +20 points in the game has over 50 hits.
+7. +30 points in the game has over 70 hits.
+8. +50 points if the game has over 100 hits.
+9. +50 points if the game was within 1 goal, but didn't go to overtime.
+10. +50 points in if the game has an empty net goal by a fav team.
+11. +50 points if the game goes to overtime because of an empty net goal.
+12. +50 points if the game has over 5 goals for each team.
+13. +50 points if the game has over 30 shots for each team.
+14. +50 points if the game has over 50 shots for a fav team.
+15. +50 points if a team comes back by over 3 goals, but the team they beat is not a fav team.
+16. +50 points if there was more than 2 fights in the game.
+*/
 
 export const calculateGameScore = (game: NHLGameScore, favoriteTeams: string[]): RankingResult => {
   let score = 0;
